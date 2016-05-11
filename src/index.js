17ea3f556895b42017b527ee21a3b61622dab21e
xenonmolecule <michael_ryan_2000@yahoo.com>
2016-05-11 20:01:32 +0000

    added socket.io and js connection

--- src/index.js
+++ src/index.js
@@ -2,8 +2,11 @@
 
 var express = require('express');
 var questions =require('./mock/questions.json');
-
 var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+var clientType = "";
 
 app.use('/static', express.static(__dirname + '/public'));
 
@@ -12,14 +15,15 @@ app.set('view engine', 'jade');
 
 //CHANGE LATER, HERE FOR C9
 var port = process.env.PORT;
-app.listen(port,function(){
+http.listen(port,function(){
     console.log("The process is running on port:"+port);
 });
 
 //HOME ROUTE--
 //      Should be the user game information like sign on and buttons to press
 app.get("/",function(req,res){
-   res.render("client.jade");
+    clientType = "player";
+    res.render("client.jade", {clientType: clientType});
 });
 
 //HOST ROUTE--
@@ -28,8 +32,17 @@ app.get("/host/:password?",function(req,res){
     //Confirms the password, that is clearly here, and unhidden, 
     //so as to be accessed by the public eye (The security is really strong on this project)
     if(req.params.password === "admin54"){
+        clientType = "host";
         res.send("Welcome Admin");
     } else {
         res.send("Unfortunately you need permision to view this page... Sorry");
     }
+});
+
+//SOCKET IO CONNECTION HANDLING
+io.on('connection', function(socket){
+  console.log('a user connected');
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
 });
